6f22fb0eccfed958526fb43db9e9055ea1f63dc5
georg0n <abashkin.george@yandex.ru>
2023-09-25 18:10:30 +0300

    It works!

--- main.py
+++ main.py
@@ -1,76 +1,113 @@
-import argparse
-import os
-import tarfile
-import zipfile
-
-class VShell:
-    def __init__(self, fs_archive):
-        self.fs_archive = fs_archive
-        self.current_dir = '/'
-        self.load_filesystem()
-
-    def load_filesystem(self):
-        if tarfile.is_tarfile(self.fs_archive):
-            with tarfile.open(self.fs_archive, 'r') as tar:
-                tar.extractall()
-
-        elif zipfile.is_zipfile(self.fs_archive):
-            with zipfile.ZipFile(self.fs_archive, 'r') as zipf:
-                zipf.extractall()
-
-    def run_command(self, command):
-        if command.startswith("pwd"):
-            print(self.current_dir)
-        elif command.startswith("ls"):
-            self.list_directory()
-        elif command.startswith("cd"):
-            self.change_directory(command.split(" ")[1])
-        elif command.startswith("cat"):
-            self.cat_file(command.split(" ")[1])
-
-    def list_directory(self):
-        with os.scandir(self.current_dir) as entries:
-            for entry in entries:
-                print(entry.name)
+from zipfile import ZipFile
+import sys
 
 
-    def change_directory(self, directory):
-        new_dir = os.path.join(self.current_dir, directory)
-        if os.path.exists(new_dir) and os.path.isdir(new_dir):
-            self.current_dir = new_dir
+def delete_symbol(path):
+    '''Обрезает абсолютный путь'''
+    for letter in path:
+        if letter == "/":
+            path = path[1:]
         else:
-            print(f"Directory '{new_dir}' does not exist.")
+            break
+    return path
 
-    def cat_file(self, filename):
-        file_path = os.path.join(self.current_dir, filename)
-        if os.path.exists(file_path) and os.path.isfile(file_path):
-            with open(file_path, 'r') as file:
-                content = file.read()
-                print(content)
-        else:
-            print(f"File '{file_path}' does not exist.")
 
-    def run_script(self, script_file):
-        with open(script_file, 'r') as script:
-            for line in script:
-                self.run_command(line.strip())
+def ls(path, files):
+    """Функция ls как в bash Linux"""
+    path = delete_symbol(path)
+    for file in files:
+        if path in file.filename:
+            file_names = file.filename[len(path):].split("/")  # разбиение имен которые идут после
+            # пути, в котором мы находимся
+            file_names = list(filter(None, file_names))  # удаление пустых строк из списка
+            if len(file_names) > 1 or not file_names:  # пропускаем повторы
+                continue
+            print("\033[33m{}\033[0m".format(file_names[0]))
 
-def main():
-    parser = argparse.ArgumentParser(description="vshell - Virtual Shell Emulator")
-    parser.add_argument("fs_archive", help="Filesystem archive (tar or zip)")
-    parser.add_argument("--script", help="Script file to execute")
-    args = parser.parse_args()
 
-    vshell = VShell(args.fs_archive)
+def cd(path, extension_path, files):
+    """Функция cd как в bash Linux"""
 
-    if args.script:
-        vshell.run_script(args.script)
+    if "root:" in extension_path:
+        '''Обработка случая если введен абсолютный путь до файла'''
+        path = extension_path[len("root:"):]
     else:
-        while True:
-            command = input("$ ")
-            if command.lower() == "exit":
-                break
-            vshell.run_command(command)
+        path += "/" + extension_path
+    path = delete_symbol(path)
 
-if __name__ == "__main__":
-    main()
+    global local_path
+
+    if path == "":
+        local_path = ""
+        return True
+
+    if "../" in path:
+        local_path = local_path[:len(local_path) - len(local_path.split("/")[-1]) - 1]
+        return True
+
+    for file in files:
+        if path in file.filename:
+            local_path = "/" + path
+            return True
+    return False
+
+
+def cat(path, ext_path, zip_file):
+    """Функция cat как в bash Linux"""
+
+    if "root:" in ext_path:
+        '''Обработка случая если введен абсолютный путь до файла'''
+        path = ext_path[len("root:"):]
+    else:
+        path += "/" + ext_path
+    path = delete_symbol(path)
+
+    flag = False
+    for file in ZipFile(zip_file).filelist:
+        if path in file.filename:
+            flag = True
+            with ZipFile(zip_file) as files:
+                with files.open(path, 'r') as file:
+                    for line in file.readlines():
+                        print(line.decode('utf8').strip())
+    if not flag:
+        print("\033[31m{}\033[0m".format("Can`t open this file"))
+
+
+if __name__ == '__main__':
+    try:
+        sys.argv[1]
+    except IndexError:
+        exit(0)
+    zipfile = ZipFile(sys.argv[1])
+    ROOT_PATH = "root:"
+    local_path = ""
+    command = input(ROOT_PATH + "/> ")
+    all_files = zipfile.filelist
+    while command != "exit":
+        command = command.split(" ")
+
+        if command[0] == "pwd":
+            # ("/" if not local_path else local_path) проверка пуст ли local_path
+            print("\033[32m{}\033[0m".format("  " + ROOT_PATH + ("/" if not local_path else local_path)))
+
+        elif command[0] == "ls":
+            ls(local_path, all_files)
+
+        elif command[0] == "cd":
+            try:
+                command[1]
+            except IndexError:
+                print("\033[31m{}\033[0m".format("Don`t know this command"))
+            if cd(local_path, command[1], all_files):
+                pass
+            else:
+                print("\033[31m{}\033[0m".format("The path does not exist"))
+
+        elif command[0] == "cat":
+            cat(local_path, command[1], sys.argv[1])
+
+        else:
+            print("\033[31m{}\033[0m".format("Don`t know this command"))
+
+        command = input(ROOT_PATH + ("/" if not local_path else local_path) + "> ")
